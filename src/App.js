318c7c5625343e3105e10ddce575fb98ebdc425a
iekei <144453060+iekei@users.noreply.github.com>
2026-07-09 10:56:21 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './pages/Navbar';
-import LineupSite from './pages/LineupSite'; 
-import Info from './pages/Info';
-import DesignLineup from './pages/DesignLineup';
-import SelectLineupPage from './pages/SelectLineupPage';
-import EditLineup from './pages/EditLineup';
+import Navbar from 'pages/Navbar';
+import LineupSite from 'pages/LineupSite'; 
+import Info from 'pages/Info';
+import DesignLineup from 'pages/DesignLineup';
+import SelectLineupPage from 'pages/SelectLineupPage';
+import EditLineup from 'pages/EditLineup';
 import AboutMenu from './AboutMenuComponent';
 
 function App() {
